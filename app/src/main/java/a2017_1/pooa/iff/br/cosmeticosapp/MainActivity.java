eaef0b3adcd7a02e4cb9da2e162423e38898f579
Caio Medina <caio__medina@hotmail.com>
2017-06-28 12:32:53 -0300

    Iniciando Projeto

---
+++ app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp/MainActivity.java
@@ -0,0 +1,13 @@
+package a2017_1.pooa.iff.br.cosmeticosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
